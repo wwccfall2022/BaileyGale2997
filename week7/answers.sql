6b65dec094e4cbe255cb75614ffe4bb5a35346ec
BaileyGale2997 <113929458+BaileyGale2997@users.noreply.github.com>
2022-12-06 20:01:30 -0800

    Update answers.sql

--- week7/answers.sql
+++ week7/answers.sql
@@ -8,3 +8,7 @@ CREATE TABLE players (
   last_name VARCHAR(30) NOT NULL, 
   email VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE characters(
+  character_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+);
