d37961f122ac4856bef2042b2c442630dfe2c275
BaileyGale2997 <113929458+BaileyGale2997@users.noreply.github.com>
2022-12-06 20:49:46 -0800

    characters table fixed

--- week7/answers.sql
+++ week7/answers.sql
@@ -9,3 +9,16 @@ CREATE TABLE players (
   email VARCHAR(50) NOT NULL
 );
 
+
+CREATE TABLE characters (
+  character_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  player_id INT UNSIGNED NOT NULL, 
+  name VARCHAR(30) NOT NULL, 
+  level TINYINT UNSIGNED NOT NULL,
+  CONSTRAINT characters_fk_players
+    FOREIGN KEY(player_id)
+      REFRENCES players(player_id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
+  
+);
